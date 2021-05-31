391233e67c503b12df539ca5041bdc1f8dbf37ab
liwei <liwei6516@163.com>
2021-06-01 00:04:27 +0800

    feat: add queryString and url package

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const http = require('http');
+const querystring = require('querystring');
+const urlLib = require('url');
 
 const server = http.createServer(function (req, res) {
-  res.write("hello node")
-  res.write("create node server")
-  res.end()
+  var str = "";
+  req.on("data", function (data) {
+    str += data;
+  })
+  req.on("end", function () {
+    const obj = urlLib.parse(req.url, true)
+    const url = obj.pathname;
+    const GET = obj.query;
+    const POST = querystring.parse(str);
+
+    console.log("url", url);
+    console.log("GET", GET);
+    console.log("POST", GET);
+    res.end()
+  })
 })
 
 server.listen(8080)
